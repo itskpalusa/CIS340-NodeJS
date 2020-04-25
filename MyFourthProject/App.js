b01d11242b7c37c87027f8fd5e6eed02cba40291
Karthik Palusa <karthik.palusa@gmail.com>
2020-04-25 17:31:36 -0600

    Week 14 Project Four

---
+++ MyFourthProject/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, Image, ScrollView } from 'react-native';
+
+const dog = {
+  uri:
+    'https://raw.githubusercontent.com/AbdunabiRamadan/CIS340/master/images/dog2.png',
+  width: 64,
+  height: 64,
+};
+
+export default MyScrollViewApp = () => (
+  <ScrollView style={{ padding: 40 }}>
+    <Text style={{ fontSize: 80 }}>Try to scroll down!</Text>
+    <Image
+      source={require('./assets/dog2.png')}
+      style={{ width: 40, height: 40 }}
+    />
+    <Image source={dog} /> <Image source={dog} /> <Image source={dog} />{' '}
+    <Image source={dog} /> <Image source={dog} /> <Image source={dog} />{' '}
+    <Text style={{ fontSize: 80 }}>
+      Try to scroll down again, if you'd like!
+    </Text>
+  </ScrollView>
+);
